8267d7d613a4ea3f675973fe6d594579802afa41
Rajib Paudyal <rajib.mp@gmail.com>
2014-05-14 13:59:20 +0000

    automatic conversion of yaml data to json

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
-var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	livereload = require('gulp-livereload'),
-	prefix = require('gulp-autoprefixer'),
-	minifycss = require('gulp-minify-css'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-ngmin'),
-	shell = require('gulp-shell');
-
+var gulp        = require('gulp'),
+    gutil       = require('gulp-util'),
+    livereload  = require('gulp-livereload'),
+    prefix      = require('gulp-autoprefixer'),
+    minifycss   = require('gulp-minify-css'),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-ngmin'),
+    shell       = require('gulp-shell'),
+    map         = require('map-stream'),
+    yaml        = require('js-yaml');
+  
 gulp.task('jekyll', function(){
 	gulp.src('')
 	.pipe(shell([
@@ -36,13 +38,35 @@ gulp.task('minify', function () {
 		.pipe(gulp.dest('css'))
 });
 
+gulp.task('jsonify', function(){
+	gulp.src('_data/**/*.yml')
+	.pipe(map(function(file,cb){
+      if (file.isNull()) return cb(null, file); // pass along
+      if (file.isStream()) return cb(new Error("Streaming not supported"));
+ 
+      var json;
+ 
+      try {
+        json = yaml.load(String(file.contents.toString('utf8')));
+      } catch(e) {
+        console.log(e);
+        console.log(json);
+      }
+ 
+      file.path = gutil.replaceExtension(file.path, '.json');
+      file.contents = new Buffer(JSON.stringify(json));
+ 
+      cb(null,file);
+    }))
+  .pipe(gulp.dest('json'))
+});
+
 gulp.task('watch', function () {
 	gulp.watch('_js/*.js', ['scripts']);
 	gulp.watch('_sass/**/*.scss', ['compass']);
 	gulp.watch('_css/*.css', ['minify']);
+  gulp.watch('_data/**/*.yml', ['jsonify']);
 });
 
 
 gulp.task('default', ['watch', 'jekyll', 'compass']);
-
-

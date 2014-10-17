c15cc623658e5c6be5cd41473985830029b0bc37
Rajib Paudyal <rajibmp@users.noreply.github.com>
2014-10-17 11:12:45 +0300

    added level as per Readme file

--- _includes/codes/order-translations.php
+++ _includes/codes/order-translations.php
@@ -8,11 +8,10 @@ $client = new Transfluent('example@example.org', 'my-password');
 
 try {
     $response = $client->SaveIosStringsFile('my-project/Localizable.strings', 1, '/home/john/work/my-project/resources/Localizable.strings');
-    echo "The file contains {$response->word_count} words." . PHP_EOL;
-    $response = $client->FileTranslate('my-project/Localizable.strings', 1, array(11), 'This is description of My-project etc.', 'http://www.example.org/callback-me.php');
-    echo "{$response->word_count} words (for all target languages) were ordered." . PHP_EOL;
-}
-catch (Exception $e) {
+    echo "The file contains {$response['word_count']} words." . PHP_EOL;
+    $response = $client->FileTranslate('my-project/Localizable.strings', 1, array(11), 'This is description of My-project etc.', 'http://www.example.org/callback-me.php', BackendClient::LEVEL_PRO_PROOF_READ);
+    echo "{$response['word_count']} words (for all target languages) were ordered." . PHP_EOL;
+} catch (Exception $e) {
     error_log($e->getMessage());
     exit;
 }

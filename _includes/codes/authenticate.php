975ef66020348a1e0c84db4d7f4c6c96e866fd3e
Tomi Heiskanen <tomi.heiskanen@transfluent.com>
2015-08-13 16:50:28 +0300

    Update Authenticate example to match new PHP client

--- _includes/codes/authenticate.php
+++ _includes/codes/authenticate.php
@@ -2,9 +2,22 @@
 <?php
 require "BackendClient.php";
 
-use Transfluent\BackendClient\BackendClient as Transfluent;
+use Transfluent\BackendClient as Transfluent;
 
 $client = new Transfluent('example@example.org', 'my-password');
-// Now once you call some method, the TF client will automatically authenticate
+try {
+    $token = $client->GetToken();
+    // Safely store $token
+} catch (Exception $e) {
+    // Handle failures..
+}
+// Ready to make some API calls!
+
+
+
+// Next time initiating the client, you can avoid additional authentication calls by setting the token using SetToken()
+$client = new Transfluent();
+$client->SetToken($token);
+// Ready to make some API calls!
 
 {% endhighlight %}

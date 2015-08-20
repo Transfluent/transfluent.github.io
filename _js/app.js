51b7de965bef0d3e15716c684fc56841f2d3154c
Tomi Heiskanen <tomi.heiskanen@transfluent.com>
2015-08-20 10:35:32 +0300

    Navigation is now translated

--- _js/app.js
+++ _js/app.js
@@ -9,10 +9,46 @@ angular.module('transfluentAPI', [
     $interpolateProvider.startSymbol('{[').endSymbol(']}');
 
     $translateProvider.translations('en', {
-        "Get started": 'Get started'
+        "Search the documentation": "Search the documentation",
+        "Get started": 'Get started',
+        "Authentication": "Authentication",
+        "Retrieve a token": "Retrieve a token",
+        "Languages": "Languages",
+        "List languages&pairs": "List languages&pairs",
+        "Text&HTML": "Text&HTML",
+        "Text": "Text",
+        "TextStatus": "TextStatus",
+        "Texts": "Texts",
+        "TextsTranslate": "TextsTranslate",
+        "Resource files": "Resource files",
+        "FileRead": "FileRead",
+        "FileSave": "FileSave",
+        "FileStatus": "FileStatus",
+        "FileTranslate": "FileTranslate",
+        "Development": "Development",
+        "Testing": "Testing",
+        "Callbacks": "Callbacks"
     });
     $translateProvider.translations('ja', {
-        "Get started": 'はじめに'
+        "Search the documentation": "Search the documentation",
+        "Get started": 'はじめに',
+        "Authentication": "認証",
+        "Retrieve a token": "トークンの取得",
+        "Languages": "言語",
+        "List languages&pairs": "言語＆組み合わせのリスト",
+        "Text&HTML": "テキスト＆HTML",
+        "Text": "テキスト",
+        "TextStatus": "テキストステータス",
+        "Texts": "テキスト",
+        "TextsTranslate": "テキスト翻訳",
+        "Resource files": "リソースファイル",
+        "FileRead": "ファイル読み込み",
+        "FileSave": "ファイル保存",
+        "FileStatus": "ファイルステータス",
+        "FileTranslate": "ファイル翻訳",
+        "Development": "開発",
+        "Testing": "テスト",
+        "Callbacks": "コールバック"
     });
     $translateProvider.preferredLanguage('en');
 })

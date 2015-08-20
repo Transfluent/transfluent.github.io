51d0f7af91f6e4043b3708259c810de9b99b268b
Tomi Heiskanen <tomi.heiskanen@transfluent.com>
2015-08-20 10:18:53 +0300

    * Add language selection to header   * Import Angular Translate into the project   * Import first translation key (for testing)

--- _js/app.js
+++ _js/app.js
@@ -1,9 +1,23 @@
 'use strict';
 
 angular.module('transfluentAPI', [
-  'transfluentAPI.controllers',
-  'transfluentAPI.filters'
+    'transfluentAPI.controllers',
+    'transfluentAPI.filters',
+    'pascalprecht.translate'
 ])
-.config(function($interpolateProvider) {
-  $interpolateProvider.startSymbol('{[').endSymbol(']}');
+.config(function($interpolateProvider, $translateProvider) {
+    $interpolateProvider.startSymbol('{[').endSymbol(']}');
+
+    $translateProvider.translations('en', {
+        "Get started": 'Get started'
+    });
+    $translateProvider.translations('ja', {
+        "Get started": 'はじめに'
+    });
+    $translateProvider.preferredLanguage('en');
+})
+.controller('LanguageSelector', function ($scope, $translate) {
+    $scope.ChangeLanguage = function (key) {
+        $translate.use(key);
+    };
 });

c5662bb529df55d6155e1c951c3fd5c51ec3d17e
Tomi Heiskanen <tomi.heiskanen@transfluent.com>
2015-08-20 11:20:19 +0300

    Translations for home page

--- _js/B-app.js
+++ _js/B-app.js
@@ -30,7 +30,9 @@ angular.module('transfluentAPI', [
         "FileTranslate": "FileTranslate",
         "Development": "Development",
         "Testing": "Testing",
-        "Callbacks": "Callbacks"
+        "Callbacks": "Callbacks",
+        "home-main-copy": "Transfluent is the only translation provider to scale from 1 word into millions of words, yet providing excellent quality and having ability to translate into numerous of languages with very little effort from the customer.",
+        "home-main-beef": "The key to fulfill this promise is our innovative use of technology in the translation process. Our API is in the core enabling all this. In this documentation you will find detailed information of our API and description how to the translation process works."
     });
     $translateProvider.translations('ja', {
         "Japanese": "Japanese",
@@ -53,7 +55,9 @@ angular.module('transfluentAPI', [
         "FileTranslate": "ファイル翻訳",
         "Development": "開発",
         "Testing": "テスト",
-        "Callbacks": "コールバック"
+        "Callbacks": "コールバック",
+        "home-main-copy": "Transfluentは、1単語から数百万単語まで、お客様にほとんど負担をかけずに優れた品質と数多くの言語への翻訳を可能にする唯一の翻訳業者です。",
+        "home-main-beef": "これを実現するキーポイントは、翻訳過程における当社の革新的な技術の利用方法です。これはAPIを核とすることで実現しています。この資料は、当社のAPIおよび翻訳過程がどう行われるかに関する詳細な情報 をお届けします。"
     });
     $translateProvider.preferredLanguage('en');
     $translateProvider.useCookieStorage();

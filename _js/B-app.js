c16f52815742cb2c9d35ebb701ab6d1a9539b0b4
Tomi Heiskanen <tomi.heiskanen@transfluent.com>
2015-08-20 11:55:34 +0300

    Translations for home page

--- _js/B-app.js
+++ _js/B-app.js
@@ -32,7 +32,10 @@ angular.module('transfluentAPI', [
         "Testing": "Testing",
         "Callbacks": "Callbacks",
         "home-main-copy": "Transfluent is the only translation provider to scale from 1 word into millions of words, yet providing excellent quality and having ability to translate into numerous of languages with very little effort from the customer.",
-        "home-main-beef": "The key to fulfill this promise is our innovative use of technology in the translation process. Our API is in the core enabling all this. In this documentation you will find detailed information of our API and description how to the translation process works."
+        "home-main-beef": "The key to fulfill this promise is our innovative use of technology in the translation process. Our API is in the core enabling all this. In this documentation you will find detailed information of our API and description how to the translation process works.",
+        "home-main-get-started": "All of our implementations are built on our API. The translation process over our API is straight forward and simple: authenticate, submit content, place the order and receive the translation (via an API call or by receiving a callback).",
+        "home-main-user-accounts-title": "User accounts",
+        "home-main-user-accounts-body": "To get going, you will need a Transfluent user account. Either <a href=\"https://www.transfluent.com/en/sign-up/\" target=\"_blank\">create a new user account</a> by yourself or ask your company's account owner to create one for you."
     });
     $translateProvider.translations('ja', {
         "Japanese": "Japanese",
@@ -57,7 +60,10 @@ angular.module('transfluentAPI', [
         "Testing": "テスト",
         "Callbacks": "コールバック",
         "home-main-copy": "Transfluentは、1単語から数百万単語まで、お客様にほとんど負担をかけずに優れた品質と数多くの言語への翻訳を可能にする唯一の翻訳業者です。",
-        "home-main-beef": "これを実現するキーポイントは、翻訳過程における当社の革新的な技術の利用方法です。これはAPIを核とすることで実現しています。この資料は、当社のAPIおよび翻訳過程がどう行われるかに関する詳細な情報 をお届けします。"
+        "home-main-beef": "これを実現するキーポイントは、翻訳過程における当社の革新的な技術の利用方法です。これはAPIを核とすることで実現しています。この資料は、当社のAPIおよび翻訳過程がどう行われるかに関する詳細な情報 をお届けします。",
+        "home-main-get-started": "当社の実装は全て当社API上に構築されています。当社APIにおける翻訳過程は分かりやすくシンプルです―認証、翻訳内容を送信、発注、翻訳結果の受け取り（APIコールあるいはコールバックの受け取りによる）、これだけです。",
+        "home-main-user-accounts-title": "ユーザーアカウント",
+        "home-main-user-accounts-body": "利用には、Transfluentのユーザーアカウントが必要になります。新しいユーザーアカウントをご自身で作成するか、御社のアカウント所有者に作成を依頼してください。"
     });
     $translateProvider.preferredLanguage('en');
     $translateProvider.useCookieStorage();
